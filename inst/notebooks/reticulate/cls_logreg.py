47970d28b1a81aea1bc6ae3685bbcd0b8bc7f0a9
alfonso <alfonso.r.reyes@gmail.com>
2017-12-15 14:15:43 -0600

    more examples using Python and R with reticulate. could not use cls_logreg.py

---
+++ inst/notebooks/reticulate/cls_logreg.py
@@ -0,0 +1,9 @@
+# Model
+class LogisticRegression(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(LogisticRegression, self).__init__()
+        self.linear = nn.Linear(input_size, num_classes)
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out
