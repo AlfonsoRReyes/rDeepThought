86306fd3c24743b31550469597696160de2032ca
f0nzie zbook G4 <alfonso.reyes@oilgainsanalytics.com>
2019-04-09 11:20:04 -0500

    move folder out

--- notebooks/py_to_r-reticulate/flights.py
+++
@@ -1,8 +0,0 @@
-import pandas
-
-def read_flights(file):
-    flights = pandas.read_csv(file)
-    flights = flights[flights["DESTINATION_AIRPORT"] == "ORD"]
-    flights = flights[["AIRLINE", "DEPARTURE_DELAY", "ARRIVAL_DELAY"]]
-    flights = flights.dropna()
-    return flights
